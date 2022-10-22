78bf1c181a629f49f5d87a9f4992615a754a78a0
xiaoyuan <95021019+mengtaoai@users.noreply.github.com>
2022-10-22 16:10:43 +0800

    Update UserServiceImpl.java

--- src/main/java/com/dragon/interview/user/service/UserServiceImpl.java
+++ src/main/java/com/dragon/interview/user/service/UserServiceImpl.java
@@ -6,8 +6,8 @@ import com.dragon.interview.user.mapper.UserMapper;
 import com.dragon.interview.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -19,29 +19,31 @@ import java.util.List;
  */
 @Service
 public class UserServiceImpl implements UserService {
-    @Resource
+    @Autowired
     UserMapper userMapper;
-    //添加用户
-    @Override
-    public int insertUser(User user) {
-        String userName = user.getUserName ();
-        User name = userMapper.getUserName (userName);
-        if (name==null){
-            int number = userMapper.insertUser (user);
 
-            return number;
+    //md5加密规则
+    private String getMd5(String userName,String userPass){
+        for (int i = 0; i <3 ; i++) {
+            userPass = DigestUtils.md5DigestAsHex((userPass ).getBytes()).toUpperCase();
         }
-        return 0;
+        return userPass;
+    }
+    //==========后台=========
 
+    //根据Id进行查询
+    @Override
+    public User getUserId(Integer userId) {
+        User user = userMapper.getUserId (userId);
+        return user;
     }
 
+    //根据姓名进行查询
     @Override
     public User getUserName(String userName) {
         User name = getUserName (userName);
         return name;
     }
-
-    //根据姓名进行查询
 
     //根据姓名进行模糊查询
     @Override
@@ -49,12 +51,14 @@ public class UserServiceImpl implements UserService {
         List<User> user = userMapper.getUserByLike (userName);
         return user;
     }
+
     //查询所有用户
     @Override
     public List<User> getAllUser() {
         List<User> allUser = userMapper.getAllUser ();
         return allUser;
     }
+
     //批量删除
     @Override
     public int deleteUsersId(Integer[] userIds) {
@@ -68,10 +72,53 @@ public class UserServiceImpl implements UserService {
         int user = userMapper.deleteUserId (userId);
         return user;
     }
+
     //修改性别
     @Override
     public int updateUserSex(Integer userId, char sex) {
         int userSex = userMapper.updateUserSex (userId, sex);
         return userSex;
     }
+    //=========前台=============
+    //注册用户
+    @Override
+    public int insertUser(User user) {
+        String userName = user.getUserName ();
+        User name = userMapper.getUserName (userName);
+        String md5Password = getMd5(user.getUserPass (),user.getUserName ());
+        user.setUserPass (md5Password);
+        if (name==null){
+            int number = userMapper.insertUser (user);
+            return number;
+        }
+        return 0;
+    }
+
+
+    //登录验证
+    @Override
+    public User login(String userName, String userPass) {
+        String md5Password = getMd5(userName,userPass);
+        if (userName!=null){
+            User login = userMapper.login (userName, md5Password);
+            return login;
+        }
+        return null;
+    }
+
+    //用户修改
+    @Override
+    public int updateUser(User user) {
+        String userName = user.getUserName ();
+        int i = userMapper.updateUser ( user);
+        String md5Password = getMd5(user.getUserPass (),user.getUserName ());
+        user.setUserPass (md5Password);
+        if (user.getUserName ()!=null){
+            int number = userMapper.updateUser (user);
+            return number;
+        }
+        return 0;
+    }
+
 }
+
